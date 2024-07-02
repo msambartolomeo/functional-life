8b79ea66f47bca400a99e2c801986982b10504dc
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2024-07-02 20:49:59 -0300

    game of life working

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,10 @@
 module Main where
 
-import Data.Array.Repa (Array, D, DIM1, DIM2, Shape, U, Z (..), (:.) (..))
+-- import Debug.Trace qualified as Debug
+import Data.Array.Repa (Array, D, DIM1, DIM2, Source, U, Z (..), (:.) (..))
 import Data.Array.Repa qualified as Repa
-import Data.Array.Repa.Repr.Vector (V)
 import Data.Array.Repa.Repr.Vector qualified as Repa
 import Data.Vector qualified as Vector
-import Debug.Trace qualified as Debug
 import Foreign.C (CInt)
 import SDL qualified
 import Sdl qualified
@@ -14,11 +13,9 @@ import Types (Life)
 raw :: [Life]
 raw =
   [ 
-    True, True,  True,  True,  True,
-    True, False, False, False, True,
-    True, False, False, False, True,
-    True, False, False, False, True,
-    True, True,  True,  True,  True
+    False, False, True,
+    True,  False, True,
+    False, True,  True
   ]
 
 size :: Int
@@ -34,10 +31,10 @@ pixelSize :: Int
 pixelSize = 10
 
 width :: Int
-width = snd resolution `div` pixelSize
+width = fst resolution `div` pixelSize
 
 height :: Int
-height = fst resolution `div` pixelSize
+height = snd resolution `div` pixelSize
 
 cellCount :: Int
 cellCount = height * width
@@ -45,81 +42,65 @@ cellCount = height * width
 emptyMatrix :: Array U DIM2 Life
 emptyMatrix = Repa.fromListUnboxed (Z :. height :. width) $ take cellCount $ False <$ [0 :: Int ..]
 
-addLife :: Array U DIM2 Life -> Array U DIM2 Life -> Array D DIM2 Life
-addLife xs ys | Debug.trace (show xs ++ "\n" ++ show ys) False = undefined
-addLife xs ys =
-  Repa.traverse2
-    xs
-    ys
-    (\_ x -> x)
-    func
-
-func :: (DIM2 -> Life) -> (((Z :. Int) :. Int) -> Bool) -> ((Z :. Int) :. Int) -> Bool
-func _ _ (Z :. i :. j) | Debug.trace ("(" ++ show i ++ ", " ++ show j ++ ")") False = undefined
-func f g (Z :. i :. j) =
-  let x = safeIndex f i j
-      y = g (Z :. i :. j)
-   in x || y
+addLives :: Array U DIM2 Life -> Array U DIM2 Life -> Array D DIM2 Life
+addLives xs ys = Repa.traverse2 xs ys const (\f g i -> safe g i || f i)
+  where
+    (Z :. h :. w) = Repa.extent ys
+    safe g (Z :. j :. i) = safeIndex (w, h) g i j
 
 board :: Array U DIM2 Life
-board = Repa.computeS $ addLife matrix emptyMatrix
-
-timeBetweenFrames :: Double
-timeBetweenFrames = 0.5
+board = Repa.computeS $ addLives emptyMatrix matrix
 
 main :: IO ()
 main = Sdl.withSdl "Functional Life" resolution $ flip runLife board
 
-runLife :: Sdl.Sdl -> Array U DIM2 Life -> IO ()
+runLife :: (Source d Life) => Sdl.Sdl -> Array d DIM2 Life -> IO ()
 runLife g b = do
   Sdl.clearScreen g
 
-  rectangles <- computeVector $ getRectanglesFromLife b
-
-  print $ length $ Vector.catMaybes $ Repa.toVector rectangles
+  rectangles <- Repa.computeVectorP $ getRectanglesFromLife b
 
   Sdl.fillRectangles g $ Vector.catMaybes $ Repa.toVector rectangles
 
   b' <- Repa.computeUnboxedP $ processLives b
 
-  SDL.delay 2000
+  SDL.delay 50
 
   Sdl.present g
 
   runLife g b'
 
-computeVector :: (Shape a) => Array D a b -> IO (Array V a b)
-computeVector = Repa.computeP
-
-getRectanglesFromLife :: Array U DIM2 Life -> Array D DIM1 (Maybe (SDL.Rectangle CInt))
-getRectanglesFromLife arr = Repa.reshape (Z :. cellCount) (Repa.traverse arr id (\l (Z :. i :. j) -> rectangle (l (Z :. i :. j)) (i, j)))
+getRectanglesFromLife :: (Source d Life) => Array d DIM2 Life -> Array D DIM1 (Maybe (SDL.Rectangle CInt))
+getRectanglesFromLife arr = Repa.reshape (Z :. cellCount) (Repa.traverse arr id (\l (Z :. j :. i) -> rectangle (l (Z :. j :. i)) (i, j)))
 
 rectangle :: Life -> (Int, Int) -> Maybe (SDL.Rectangle CInt)
 rectangle True (x, y) = Just $ SDL.Rectangle (SDL.P $ fromIntegral <$> SDL.V2 (y * pixelSize) (x * pixelSize)) (fromIntegral <$> SDL.V2 pixelSize pixelSize)
 rectangle False _ = Nothing
 
-processLives :: Array U DIM2 Life -> Array D DIM2 Life
+processLives :: (Source d Life) => Array d DIM2 Life -> Array D DIM2 Life
 processLives xs = Repa.traverse xs id processLife
 
 processLife :: (DIM2 -> Life) -> DIM2 -> Life
-processLife f (Z :. i :. j) = liveOrDie c $ (length . filter id) [n, ne, e, se, s, sw, w, nw]
+-- processLife f (Z :. j :. i) | Debug.trace ("(" ++ show i ++ ", " ++ show j ++ ") :: " ++ show (f (Z :. j :. i))) False  = undefined
+processLife f (Z :. j :. i) = liveOrDie c $ (length . filter id) [n, ne, e, se, s, sw, w, nw]
   where
-    c  = f (Z :. i :. j)
-    n  = safeIndex f i       (j - 1)
-    ne = safeIndex f (i + 1) (j - 1)
-    e  = safeIndex f (i + 1) j
-    se = safeIndex f (i + 1) (j + 1)
-    s  = safeIndex f i       (j + 1)
-    sw = safeIndex f (i - 1) (j + 1)
-    w  = safeIndex f (i - 1) j
-    nw = safeIndex f (i - 1) (j - 1)
+    indexer = safeIndex (width, height) f
+    c = f (Z :. j :. i)
+    n = indexer i (j - 1)
+    ne = indexer (i + 1) (j - 1)
+    e = indexer (i + 1) j
+    se = indexer (i + 1) (j + 1)
+    s = indexer i (j + 1)
+    sw = indexer (i - 1) (j + 1)
+    w = indexer (i - 1) j
+    nw = indexer (i - 1) (j - 1)
 
-safeIndex :: (DIM2 -> Life) -> Int -> Int -> Life
--- safeIndex _ i j | Debug.trace ("(" ++ show i ++ ", " ++ show j ++ ")" ) False  = undefined
-safeIndex m i j = not (i < 0 || j < 0 || i > width || j > height) && m (Z :. i :. j)
+safeIndex :: (Int, Int) -> (DIM2 -> Life) -> Int -> Int -> Life
+-- safeIndex _ _ i j      | Debug.trace ("(" ++ show i ++ ", " ++ show j ++ ")") False  = undefined
+safeIndex (w, h) m i j = not (i < 0 || j < 0 || i >= w || j >= h) && m (Z :. j :. i)
 
 liveOrDie :: Life -> Int -> Life
 -- liveOrDie b    n | Debug.trace ("This life is " ++ show b ++ " and has " ++ show n ) False  = undefined
-liveOrDie _    3 = True
+liveOrDie _ 3 = True
 liveOrDie True 2 = True
-liveOrDie _    _ = False
+liveOrDie _ _ = False
