aab6cc8ed559cd379307397590a799ac7fde7c30
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2024-07-10 21:37:23 -0300

    read text from args

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,21 @@ module Main where
 import Data.Array.Repa (Array, DIM2, Source, U, Z (..), (:.) (..))
 import Data.Array.Repa qualified as Repa
 import Data.Array.Repa.Repr.Vector qualified as Repa
+import Data.Maybe (fromMaybe)
 import Fonts (Font, fontLines, fromString)
 import GameOfLife (GameOfLife ((<.>)), processLives)
 import GameOfLife qualified as GoL
 import Printer qualified
+import ReadArgs (readArgs)
 import Sdl qualified
 import Types (Life)
 import Types qualified
 
-resolution :: (Int, Int)
-resolution = (1920, 1080)
-
-word :: [Font]
-word = fromString "GROBA"
-
--- dots = take 41 $ True <$ [0 :: Int ..]
-
 main :: IO ()
 main = do
-  Sdl.withSdl "Functional Life" resolution (\sdl -> Sdl.mainLoop sdl runLife $ GoL.move (1000, 0) $ buildLife word)
+  (text :: String, resolution :: Maybe (Int, Int)) <- readArgs
+
+  Sdl.withSdl "Functional Life" (fromMaybe (1920, 1080) resolution) (\sdl -> Sdl.mainLoop sdl runLife $ GoL.move (1000, 10) $ buildLife $ fromString text)
 
 buildLife :: (GameOfLife g) => [Font] -> g
 buildLife = foldr (\(i, bs) gol -> GoL.move (i * 92, i * 21) (Printer.createPrinter i bs) <.> gol) (GoL.empty (0, 0)) . zip [0 :: Int ..] . fontLines
@@ -30,13 +26,11 @@ runLife :: Sdl.Sdl -> Array U DIM2 Life -> IO (Array U DIM2 Life)
 runLife g b = do
   aliveCoordinates <- Repa.selectP (Types.from . Repa.linearIndex b) (coordinatesOfLinearIndex b) (Repa.size (Repa.extent b))
 
-  rectangles <- Repa.computeP $ Repa.map (Sdl.rectangle pixelSize) aliveCoordinates
+  rectangles <- Repa.computeP $ Repa.map (Sdl.rectangle 1) aliveCoordinates
 
   Sdl.fillRectangles g $ Repa.toVector rectangles
 
   Repa.computeP $ processLives b
-  where
-    pixelSize = 1
 
 {-# INLINE coordinatesOfLinearIndex #-}
 coordinatesOfLinearIndex :: (Source r e) => Array r DIM2 e -> Int -> (Int, Int)
